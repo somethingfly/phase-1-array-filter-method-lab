8783d6a3f46b27b56c6c2daf9a83825ecb015b0f
Alfred <somethingfly@somethingfly.com>
2022-06-25 18:07:00 -0700

    commit tree b67715a4bbe741a8e84f969081c7454decf2fbb3

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(driverNames, string) {
+    return driverNames.filter(function (name) {
+        return (name.toLowerCase() === string.toLowerCase());
+    });
+}
+
+function fuzzyMatch(driverNames, string) {
+    return driverNames.filter(function (name) {
+        return (name.slice(0,string.length) === string);
+    });
+}
+
+function matchName(driver, string) {
+    return driver.filter(function (element) {
+        return (element.name === string);
+    });
+}
